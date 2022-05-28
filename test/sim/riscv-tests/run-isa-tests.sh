4090f4eb24f9f0ee29353dbcbf82e28fe0aa8d8f
Luke Wren <wren6991@gmail.com>
2022-05-28 15:01:27 +0100

    Initial bringup of riscv-tests. Pass 63 out of 66 applicable tests.gstat

---
+++ test/sim/riscv-tests/run-isa-tests.sh
@@ -0,0 +1,11 @@
+set -e
+
+make -C ../tb_cxxrtl tb
+cd riscv-tests/isa
+make XLEN=32 clean
+
+# Note SKIP_V is something I added to their Makefile. We have no need for the
+# virtual memory test machine configuration.
+make -j$(nproc) XLEN=32 SKIP_V=1 rv32ui rv32uc rv32um rv32ua rv32mi
+
+for test in $(find -name "*-p-*.bin"); do echo $test; ../../../tb_cxxrtl/tb --bin $test --cycles 10000; done
