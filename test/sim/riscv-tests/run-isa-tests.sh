460fa0bb4adee44ba94c8c8dc59a4ea5e349e984
Luke Wren <wren6991@gmail.com>
2022-05-28 17:22:28 +0100

    Fix run-isa-tests.sh to fail on first failed test. Fix bad environment trap routing causing ecall ISA test to hang. Make breakpoint test instant-pass when triggers aren't implemented.

--- test/sim/riscv-tests/run-isa-tests.sh
+++ test/sim/riscv-tests/run-isa-tests.sh
@@ -8,4 +8,4 @@ make XLEN=32 clean
 # virtual memory test machine configuration.
 make -j$(nproc) XLEN=32 SKIP_V=1 rv32ui rv32uc rv32um rv32ua rv32mi
 
-for test in $(find -name "*-p-*.bin"); do echo $test; ../../../tb_cxxrtl/tb --bin $test --cycles 10000; done
+for test in $(find -name "*-p-*.bin"); do echo $test; ../../../tb_cxxrtl/tb --bin $test --cycles 10000 --vcd $test.vcd --cpuret; done
