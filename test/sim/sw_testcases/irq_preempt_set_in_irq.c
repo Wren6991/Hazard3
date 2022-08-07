2e3d69e98ffa61f920390e70b8d66dd6703c3d8c
Luke Wren <wren6991@gmail.com>
2022-08-07 22:08:50 +0100

    Forgot to add expected output for preemption test

--- test/sim/sw_testcases/irq_preempt_set_in_irq.c
+++ test/sim/sw_testcases/irq_preempt_set_in_irq.c
@@ -1,6 +1,45 @@
 #include "tb_cxxrtl_io.h"
 #include "hazard3_irq.h"
 
+/*EXPECTED-OUTPUT***************************************************************
+
+Posting first IRQ
+Entered IRQ 0
+Entered IRQ 1
+Entered IRQ 2
+Entered IRQ 3
+Entered IRQ 4
+Entered IRQ 5
+Entered IRQ 6
+Entered IRQ 7
+Entered IRQ 8
+Entered IRQ 9
+Entered IRQ 10
+Entered IRQ 11
+Entered IRQ 12
+Entered IRQ 13
+Entered IRQ 14
+Entered IRQ 15
+Exiting IRQ 15
+Exiting IRQ 14
+Exiting IRQ 13
+Exiting IRQ 12
+Exiting IRQ 11
+Exiting IRQ 10
+Exiting IRQ 9
+Exiting IRQ 8
+Exiting IRQ 7
+Exiting IRQ 6
+Exiting IRQ 5
+Exiting IRQ 4
+Exiting IRQ 3
+Exiting IRQ 2
+Exiting IRQ 1
+Exiting IRQ 0
+Done.
+
+*******************************************************************************/
+
 #define MAX_PRIORITY 15
 #define NUM_IRQS 64
 
