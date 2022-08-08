5894ddf15c4524a0dd690e62ef97e486ad5fd6ef
Luke Wren <wren6991@gmail.com>
2022-08-08 18:44:58 +0100

    Fix outdated expected output in irq_set_all_with_pri test

--- test/sim/sw_testcases/irq_set_all_with_pri.c
+++ test/sim/sw_testcases/irq_set_all_with_pri.c
@@ -18,40 +18,40 @@
 /*EXPECTED-OUTPUT***************************************************************
 
 Posting IRQs
-Entered IRQ 15
-Exiting IRQ 15
+Entered IRQ 30
+Entered IRQ 31
+Entered IRQ 28
+Entered IRQ 29
+Entered IRQ 26
+Entered IRQ 27
+Entered IRQ 24
+Entered IRQ 25
+Entered IRQ 22
+Entered IRQ 23
+Entered IRQ 20
+Entered IRQ 21
+Entered IRQ 18
+Entered IRQ 19
+Entered IRQ 16
+Entered IRQ 17
 Entered IRQ 14
-Exiting IRQ 14
-Entered IRQ 13
-Exiting IRQ 13
+Entered IRQ 15
 Entered IRQ 12
-Exiting IRQ 12
-Entered IRQ 11
-Exiting IRQ 11
+Entered IRQ 13
 Entered IRQ 10
-Exiting IRQ 10
-Entered IRQ 9
-Exiting IRQ 9
+Entered IRQ 11
 Entered IRQ 8
-Exiting IRQ 8
-Entered IRQ 7
-Exiting IRQ 7
+Entered IRQ 9
 Entered IRQ 6
-Exiting IRQ 6
-Entered IRQ 5
-Exiting IRQ 5
+Entered IRQ 7
 Entered IRQ 4
-Exiting IRQ 4
-Entered IRQ 3
-Exiting IRQ 3
+Entered IRQ 5
 Entered IRQ 2
-Exiting IRQ 2
-Entered IRQ 1
-Exiting IRQ 1
+Entered IRQ 3
 Entered IRQ 0
-Exiting IRQ 0
+Entered IRQ 1
 EIRQ vector was entered 1 times
-gstat
+
 *******************************************************************************/
 
 #define PRIORITY_LEVELS 16
@@ -87,5 +87,4 @@ void handler(void) {
 	int irqnum = h3irq_get_current_irq();
 	tb_printf("Entered IRQ %d\n", irqnum);
 	tb_clr_irq_masked(1u << irqnum);
-	tb_printf("Exiting IRQ %d\n", irqnum);
 }
