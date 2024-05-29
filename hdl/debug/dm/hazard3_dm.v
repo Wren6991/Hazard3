Wren6991/Hazard3

commit 78a5cb98ea5d184c7b8bf31249ce8ac69e4abf68

PPA: Register instruction injection output from DM, to reduce routing impact on frontend prefetch queue

hdl/debug/dm/hazard3_dm.v
@@ -62,8 +62,8 @@ module hazard3_dm #(
 	input  wire [N_HARTS-1:0]        hart_data0_wen,
 
 	// Hart instruction injection
-	output wire [N_HARTS*XLEN-1:0]   hart_instr_data,
-	output wire [N_HARTS-1:0]        hart_instr_data_vld,
+	output wire [N_HARTS*32-1:0]     hart_instr_data,
+	output reg  [N_HARTS-1:0]        hart_instr_data_vld,
 	input  wire [N_HARTS-1:0]        hart_instr_data_rdy,
 	input  wire [N_HARTS-1:0]        hart_instr_caught_exception,
 	input  wire [N_HARTS-1:0]        hart_instr_caught_ebreak,
@@ -569,7 +569,9 @@ localparam CMDERR_EXCEPTION = 3'h3;
 localparam CMDERR_HALTRESUME = 3'h4;
 
 reg [2:0]         abstractcs_cmderr;
+reg [2:0]         abstractcs_cmderr_nxt;
 reg [W_STATE-1:0] acmd_state;
+reg [W_STATE-1:0] acmd_state_nxt;
 
 assign abstractcs_busy = acmd_state != S_IDLE;
 
@@ -621,19 +623,16 @@ always @ (posedge clk or negedge rst_n) begin
 		acmd_prev_postexec <= 1'b0;
 		acmd_prev_transfer <= 1'b0;
 		acmd_prev_write <= 1'b0;
-		acmd_prev_regno <= 5'h0;
 		acmd_prev_unsupported <= 1'b1;
 	end else if (!dmactive) begin
 		acmd_prev_postexec <= 1'b0;
 		acmd_prev_transfer <= 1'b0;
 		acmd_prev_write <= 1'b0;
-		acmd_prev_regno <= 5'h0;
 		acmd_prev_unsupported <= 1'b1;
 	end else if (start_abstract_cmd && acmd_new) begin
 		acmd_prev_postexec <= acmd_new_postexec;
 		acmd_prev_transfer <= acmd_new_transfer;
 		acmd_prev_write <= acmd_new_write;
-		acmd_prev_regno <= acmd_new_regno;
 		acmd_prev_unsupported <= acmd_new_unsupported;
 	end
 end
@@ -641,8 +640,86 @@ end
 wire       acmd_postexec    = acmd_new ? acmd_new_postexec    : acmd_prev_postexec   ;
 wire       acmd_transfer    = acmd_new ? acmd_new_transfer    : acmd_prev_transfer   ;
 wire       acmd_write       = acmd_new ? acmd_new_write       : acmd_prev_write      ;
-wire [4:0] acmd_regno       = acmd_new ? acmd_new_regno       : acmd_prev_regno      ;
 wire       acmd_unsupported = acmd_new ? acmd_new_unsupported : acmd_prev_unsupported;
+
+always @ (*) begin
+	// Default: no state change
+	acmd_state_nxt = acmd_state;
+	abstractcs_cmderr_nxt = abstractcs_cmderr;
+
+	if (dmi_write && dmi_regaddr == ADDR_ABSTRACTCS && !abstractcs_busy)
+		abstractcs_cmderr_nxt = abstractcs_cmderr & ~dmi_pwdata[10:8];
+	if (abstractcs_cmderr == CMDERR_OK && abstractcs_busy && dmi_access_illegal_when_busy)
+		abstractcs_cmderr_nxt = CMDERR_BUSY;
+	if (acmd_state != S_IDLE && hart_instr_caught_exception[hartsel])
+		abstractcs_cmderr_nxt = CMDERR_EXCEPTION;
+
+	case (acmd_state)
+		S_IDLE: begin
+			if (start_abstract_cmd) begin
+				if (!hart_halted[hartsel] || !hart_available[hartsel]) begin
+					abstractcs_cmderr_nxt = CMDERR_HALTRESUME;
+				end else if (acmd_unsupported) begin
+					abstractcs_cmderr_nxt = CMDERR_UNSUPPORTED;
+				end else begin
+					if (acmd_transfer && acmd_write)
+						acmd_state_nxt = S_ISSUE_REGWRITE;
+					else if (acmd_transfer && !acmd_write)
+						acmd_state_nxt = S_ISSUE_REGREAD;
+					else if (acmd_postexec)
+						acmd_state_nxt = S_ISSUE_PROGBUF0;
+					else
+						acmd_state_nxt = S_IDLE;
+				end
+			end
+		end
+
+		S_ISSUE_REGREAD: begin
+			if (hart_instr_data_rdy[hartsel])
+				acmd_state_nxt = S_ISSUE_REGEBREAK;
+		end
+		S_ISSUE_REGWRITE: begin
+			if (hart_instr_data_rdy[hartsel])
+				acmd_state_nxt = S_ISSUE_REGEBREAK;
+		end
+		S_ISSUE_REGEBREAK: begin
+			if (hart_instr_data_rdy[hartsel])
+				acmd_state_nxt = S_WAIT_REGEBREAK;
+		end
+		S_WAIT_REGEBREAK: begin
+			if (hart_instr_caught_ebreak[hartsel]) begin
+				if (acmd_prev_postexec)
+					acmd_state_nxt = S_ISSUE_PROGBUF0;
+				else
+					acmd_state_nxt = S_IDLE;
+			end
+		end
+
+		S_ISSUE_PROGBUF0: begin
+			if (hart_instr_data_rdy[hartsel])
+				acmd_state_nxt = S_ISSUE_PROGBUF1;
+		end
+		S_ISSUE_PROGBUF1: begin
+			if (hart_instr_caught_exception[hartsel] || hart_instr_caught_ebreak[hartsel]) begin
+				acmd_state_nxt = S_IDLE;
+			end else if (hart_instr_data_rdy[hartsel]) begin
+				acmd_state_nxt = S_ISSUE_IMPEBREAK;
+			end
+		end
+		S_ISSUE_IMPEBREAK: begin
+			if (hart_instr_caught_exception[hartsel] || hart_instr_caught_ebreak[hartsel]) begin
+				acmd_state_nxt = S_IDLE;
+			end else if (hart_instr_data_rdy[hartsel]) begin
+				acmd_state_nxt = S_WAIT_IMPEBREAK;
+			end
+		end
+		S_WAIT_IMPEBREAK: begin
+			if (hart_instr_caught_exception[hartsel] || hart_instr_caught_ebreak[hartsel]) begin
+				acmd_state_nxt = S_IDLE;
+			end
+		end
+	endcase
+end
 
 always @ (posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
@@ -652,92 +729,37 @@ always @ (posedge clk or negedge rst_n) begin
 		abstractcs_cmderr <= CMDERR_OK;
 		acmd_state <= S_IDLE;
 	end else begin
-		if (dmi_write && dmi_regaddr == ADDR_ABSTRACTCS && !abstractcs_busy)
-			abstractcs_cmderr <= abstractcs_cmderr & ~dmi_pwdata[10:8];
-		if (abstractcs_cmderr == CMDERR_OK && abstractcs_busy && dmi_access_illegal_when_busy)
-			abstractcs_cmderr <= CMDERR_BUSY;
-		if (acmd_state != S_IDLE && hart_instr_caught_exception[hartsel])
-			abstractcs_cmderr <= CMDERR_EXCEPTION;
-		case (acmd_state)
-			S_IDLE: begin
-				if (start_abstract_cmd) begin
-					if (!hart_halted[hartsel] || !hart_available[hartsel]) begin
-						abstractcs_cmderr <= CMDERR_HALTRESUME;
-					end else if (acmd_unsupported) begin
-						abstractcs_cmderr <= CMDERR_UNSUPPORTED;
-					end else begin
-						if (acmd_transfer && acmd_write)
-							acmd_state <= S_ISSUE_REGWRITE;
-						else if (acmd_transfer && !acmd_write)
-							acmd_state <= S_ISSUE_REGREAD;
-						else if (acmd_postexec)
-							acmd_state <= S_ISSUE_PROGBUF0;
-						else
-							acmd_state <= S_IDLE;
-					end
-				end
-			end
-
-			S_ISSUE_REGREAD: begin
-				if (hart_instr_data_rdy[hartsel])
-					acmd_state <= S_ISSUE_REGEBREAK;
-			end
-			S_ISSUE_REGWRITE: begin
-				if (hart_instr_data_rdy[hartsel])
-					acmd_state <= S_ISSUE_REGEBREAK;
-			end
-			S_ISSUE_REGEBREAK: begin
-				if (hart_instr_data_rdy[hartsel])
-					acmd_state <= S_WAIT_REGEBREAK;
-			end
-			S_WAIT_REGEBREAK: begin
-				if (hart_instr_caught_ebreak[hartsel]) begin
-					if (acmd_prev_postexec)
-						acmd_state <= S_ISSUE_PROGBUF0;
-					else
-						acmd_state <= S_IDLE;
-				end
-			end
-
-			S_ISSUE_PROGBUF0: begin
-				if (hart_instr_data_rdy[hartsel])
-					acmd_state <= S_ISSUE_PROGBUF1;
-			end
-			S_ISSUE_PROGBUF1: begin
-				if (hart_instr_caught_exception[hartsel] || hart_instr_caught_ebreak[hartsel]) begin
-					acmd_state <= S_IDLE;
-				end else if (hart_instr_data_rdy[hartsel]) begin
-					acmd_state <= S_ISSUE_IMPEBREAK;
-				end
-			end
-			S_ISSUE_IMPEBREAK: begin
-				if (hart_instr_caught_exception[hartsel] || hart_instr_caught_ebreak[hartsel]) begin
-					acmd_state <= S_IDLE;
-				end else if (hart_instr_data_rdy[hartsel]) begin
-					acmd_state <= S_WAIT_IMPEBREAK;
-				end
-			end
-			S_WAIT_IMPEBREAK: begin
-				if (hart_instr_caught_exception[hartsel] || hart_instr_caught_ebreak[hartsel]) begin
-					acmd_state <= S_IDLE;
-				end
-			end
-		endcase
+		abstractcs_cmderr <= abstractcs_cmderr_nxt;
+		acmd_state <= acmd_state_nxt;
 	end
 end
 
-assign hart_instr_data_vld = {{N_HARTS{1'b0}},
-	acmd_state == S_ISSUE_REGREAD || acmd_state == S_ISSUE_REGWRITE || acmd_state == S_ISSUE_REGEBREAK ||
-	acmd_state == S_ISSUE_PROGBUF0 || acmd_state == S_ISSUE_PROGBUF1 || acmd_state == S_ISSUE_IMPEBREAK
+wire hart_instr_data_vld_nxt = {{N_HARTS{1'b0}},
+	acmd_state_nxt == S_ISSUE_REGREAD || acmd_state_nxt == S_ISSUE_REGWRITE || acmd_state_nxt == S_ISSUE_REGEBREAK ||
+	acmd_state_nxt == S_ISSUE_PROGBUF0 || acmd_state_nxt == S_ISSUE_PROGBUF1 || acmd_state_nxt == S_ISSUE_IMPEBREAK
 } << hartsel;
 
-assign hart_instr_data = {N_HARTS{
-	acmd_state == S_ISSUE_REGWRITE  ? 32'hbff02073 | {20'd0, acmd_prev_regno,  7'd0} : // csrr xx, dmdata0
-	acmd_state == S_ISSUE_REGREAD   ? 32'hbff01073 | {12'd0, acmd_prev_regno, 15'd0} : // csrw dmdata0, xx
-	acmd_state == S_ISSUE_PROGBUF0  ? progbuf0                                       :
-	acmd_state == S_ISSUE_PROGBUF1  ? progbuf1                                       :
-	                                  32'h00100073                                     // ebreak
-}};
+wire [31:0] hart_instr_data_nxt =
+	acmd_state_nxt == S_ISSUE_REGWRITE  ? 32'hbff02073 | {20'd0, acmd_new_regno,  7'd0} : // csrr xx, dmdata0
+	acmd_state_nxt == S_ISSUE_REGREAD   ? 32'hbff01073 | {12'd0, acmd_new_regno, 15'd0} : // csrw dmdata0, xx
+	acmd_state_nxt == S_ISSUE_PROGBUF0  ? progbuf0                                      :
+	acmd_state_nxt == S_ISSUE_PROGBUF1  ? progbuf1                                      :
+	                                      32'h00100073;                                   // ebreak
+
+reg [31:0] hart_instr_data_reg;
+assign hart_instr_data = {N_HARTS{hart_instr_data_reg}};
+
+always @ (posedge clk or negedge rst_n) begin
+	if (!rst_n) begin
+		hart_instr_data_vld <= 1'b0;
+		hart_instr_data_reg <= 32'h00000000;
+	end else begin
+		hart_instr_data_vld <= hart_instr_data_vld_nxt;
+		if (hart_instr_data_vld_nxt) begin
+			hart_instr_data_reg <= hart_instr_data_nxt;
+		end
+	end
+end
 
 // ----------------------------------------------------------------------------
 // Status helper functions

commit 799f4f2c2675ab7ac62c6b4b3ad4e9186ee12040

Fix verilator lint width issues in triggers, PMP, DM.
There was one genuine issue introduced by PPA changes in 78a5cb98e which
affected instruction injection on multiple harts from the DM (indicating
SMP debug testing needs to be part of regular automated regressions,
instead of semi-manual...). The rest are cosmetic.

hdl/debug/dm/hazard3_dm.v
@@ -357,8 +357,8 @@ always @ (posedge clk or negedge rst_n) begin
 			// a sbdata0 read with sbautoincrement=1 and sbreadondata=0, but
 			// this seems to be a typo, fixed in later versions.
 			sbaddress <= sbaddress + (
-				sbaccess[1:0] == 2'b00 ? 3'h1 :
-				sbaccess[1:0] == 2'b01 ? 3'h2 : 3'h4
+				sbaccess[1:0] == 2'b00 ? 32'd1 :
+				sbaccess[1:0] == 2'b01 ? 32'd2 : 32'd4
 			);
 		end
 	end
@@ -496,7 +496,7 @@ reg [XLEN-1:0] abstract_data0;
 assign hart_data0_rdata = {N_HARTS{abstract_data0}};
 
 always @ (posedge clk or negedge rst_n) begin: update_hart_data0
-	integer i;
+	reg signed [31:0] i;
 	if (!rst_n) begin
 		abstract_data0 <= {XLEN{1'b0}};
 	end else if (!dmactive) begin
@@ -505,7 +505,7 @@ always @ (posedge clk or negedge rst_n) begin: update_hart_data0
 		abstract_data0 <= dmi_pwdata;
 	end else begin
 		for (i = 0; i < N_HARTS; i = i + 1) begin
-			if (hartsel == i && hart_data0_wen[i] && hart_halted[i] && abstractcs_busy)
+			if (hartsel == i[W_HARTSEL-1:0] && hart_data0_wen[i] && hart_halted[i] && abstractcs_busy)
 				abstract_data0 <= hart_data0_wdata[i * XLEN +: XLEN];
 		end
 	end
@@ -739,7 +739,7 @@ always @ (posedge clk or negedge rst_n) begin
 	end
 end
 
-wire hart_instr_data_vld_nxt = {{N_HARTS{1'b0}},
+wire [N_HARTS-1:0] hart_instr_data_vld_nxt = {{N_HARTS-1{1'b0}},
 	acmd_state_nxt == S_ISSUE_REGREAD || acmd_state_nxt == S_ISSUE_REGWRITE || acmd_state_nxt == S_ISSUE_REGEBREAK ||
 	acmd_state_nxt == S_ISSUE_PROGBUF0 || acmd_state_nxt == S_ISSUE_PROGBUF1 || acmd_state_nxt == S_ISSUE_IMPEBREAK
 } << hartsel;

hdl/hazard3_pmp.v
@@ -72,7 +72,7 @@ reg [W_ADDR-3:0] pmpaddr  [0:PMP_REGIONS-1];
 reg [PMP_REGIONS-1:0] pmpcfg_m;
 
 always @ (posedge clk or negedge rst_n) begin: cfg_update
-	integer i;
+	reg signed [31:0] i;
 	if (!rst_n) begin
 		for (i = 0; i < PMP_REGIONS; i = i + 1) begin
 			pmpcfg_l[i] <= PMP_HARDWIRED[i] ? PMP_HARDWIRED_CFG[8 * i + 7]      : 1'b0;
@@ -87,7 +87,7 @@ always @ (posedge clk or negedge rst_n) begin: cfg_update
 		pmpcfg_m <= {PMP_REGIONS{1'b0}};
 	end else if (cfg_wen) begin
 		for (i = 0; i < PMP_REGIONS; i = i + 1) begin
-			if (cfg_addr == PMPCFG0 + i / 4 && !pmpcfg_l[i]) begin
+			if (cfg_addr == PMPCFG0 + i[13:2] && !pmpcfg_l[i]) begin
 				if (PMP_HARDWIRED[i]) begin
 					// Keep tied to hardwired value (but still make the "register" sensitive to clk)
 					pmpcfg_l[i] <= PMP_HARDWIRED_CFG[8 * i + 7];
@@ -108,7 +108,7 @@ always @ (posedge clk or negedge rst_n) begin: cfg_update
 						cfg_wdata[i % 4 * 8 + 3 +: 2];
 				end
 			end
-			if (cfg_addr == PMPADDR0 + i && !pmpcfg_l[i]) begin
+			if (cfg_addr == PMPADDR0 + i[11:0] && !pmpcfg_l[i]) begin
 				if (PMP_GRAIN > 1) begin
 					pmpaddr[i] <= cfg_wdata[W_ADDR-3:0] | ~(~30'h0 << (PMP_GRAIN - 1));
 				end else begin
@@ -124,10 +124,10 @@ end
 
 
 always @ (*) begin: cfg_read
-	integer i;
+	reg signed [31:0] i;
 	cfg_rdata = {W_DATA{1'b0}};
 	for (i = 0; i < PMP_REGIONS; i = i + 1) begin
-		if (cfg_addr == PMPCFG0 + i / 4) begin
+		if (cfg_addr == PMPCFG0 + i[13:2]) begin
 			cfg_rdata[i % 4 * 8 +: 8] = {
 				pmpcfg_l[i],
 				2'b00,
@@ -136,7 +136,7 @@ always @ (*) begin: cfg_read
 				pmpcfg_w[i],
 				pmpcfg_r[i]
 			};
-		end else if (cfg_addr == PMPADDR0 + i) begin
+		end else if (cfg_addr == PMPADDR0 + i[11:0]) begin
 			// If G > 1, the G-1 LSBs of pmpaddr_i are read-only-zero when
 			// region is OFF, and read-only-one when region is NAPOT.
 			if (PMP_GRAIN > 1 && !PMP_HARDWIRED[i]) begin
@@ -255,7 +255,7 @@ reg i_m; // Hazard3 extension (M-mode without locking)
 reg i_l;
 reg i_x;
 
-wire [W_ADDR-1:0] i_addr_hw1 = i_addr + 2'h2;
+wire [W_ADDR-1:0] i_addr_hw1 = i_addr + 32'd2;
 
 always @ (*) begin: check_i_match
 	integer i;

hdl/hazard3_triggers.v
@@ -55,6 +55,7 @@ end else begin: have_triggers
 parameter W_TSELECT = $clog2(BREAKPOINT_TRIGGERS);
 
 reg [W_TSELECT-1:0] tselect;
+wire tselect_in_range = {{32-W_TSELECT{1'sb0}}, $signed(tselect)} < BREAKPOINT_TRIGGERS;
 
 // Note tdata1 and mcontrol are the same CSR. tdata1 refers to the universal
 // fields (type/dmode) and mcontrol refers to those fields specific to
@@ -84,7 +85,7 @@ always @ (posedge clk or negedge rst_n) begin: cfg_update
 		end
 	end else if (cfg_wen && cfg_addr == TSELECT) begin
 		tselect <= cfg_wdata[W_TSELECT-1:0];
-	end else if (cfg_wen && tselect < BREAKPOINT_TRIGGERS && !(tdata1_dmode[tselect] && !d_mode)) begin
+	end else if (cfg_wen && tselect_in_range && !(tdata1_dmode[tselect] && !d_mode)) begin
 		// Handle writes to tselect-indexed registers (note writes to D-mode
 		// triggers in non-D-mode are ignored rather than raising an exception)
 		if (cfg_addr == TDATA1) begin
@@ -109,7 +110,7 @@ always @ (*) begin
 	if (cfg_addr == TSELECT) begin
 		cfg_rdata = {{W_DATA-W_TSELECT{1'b0}}, tselect};
 	end else if (cfg_addr == TDATA1) begin
-		if (tselect >= BREAKPOINT_TRIGGERS) begin
+		if (!tselect_in_range) begin
 			// Nonexistent -> type=0
 			cfg_rdata = {W_DATA{1'b0}};
 		end else begin
@@ -134,13 +135,13 @@ always @ (*) begin
 			};
 		end
 	end else if (cfg_addr == TDATA2) begin
-		if (tselect >= BREAKPOINT_TRIGGERS) begin
+		if (!tselect_in_range) begin
 			cfg_rdata = {W_DATA{1'b0}};
 		end else begin
 			cfg_rdata = tdata2[tselect];
 		end
 	end else if (cfg_addr == TINFO) begin
-		if (tselect >= BREAKPOINT_TRIGGERS) begin
+		if (!tselect_in_range) begin
 			cfg_rdata = 32'h00000001; // type = 0, no trigger
 		end else begin
 			cfg_rdata = 32'h00000004; // type = 2, address/data match