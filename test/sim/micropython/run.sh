6e6b87c7b8564b6726417a2f051f07bd8ea3c41b
Luke Wren <wren6991@gmail.com>
2026-08-02 15:14:01 +0100

    MicroPython: now clean on the basics/ test suite (all port config issues)

--- test/sim/micropython/run.sh
+++ test/sim/micropython/run.sh
@@ -3,6 +3,12 @@
 set -ex
 
 make -C ../tb_verilator -j$(nproc)
+make -C micropython/ports/hazard3-tests submodules
 make -C micropython/ports/hazard3-tests -j$(nproc)
+
 cd micropython/tests
-./run-tests.py -t "exec:../../../tb_verilator/tb --bin ../ports/hazard3-tests/build/firmware.bin"         
+MICROPY_TEST_TIMEOUT=300 ./run-tests.py \
+	-t "exec:../../../tb_verilator/tb --bin ../ports/hazard3-tests/build/firmware.bin" \
+	-d basics -j$(nproc) \
+	|| ./run-tests.py --print-failures
+
