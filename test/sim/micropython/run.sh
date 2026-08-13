7f2c0eaa98b0f96e41ce50adc4da2ea157fb943f
Luke Wren <wren6991@gmail.com>
2026-08-13 05:32:41 +0100

    Expand micropython test configuration further (include non-basics, RV32I emitter etc)

--- test/sim/micropython/run.sh
+++ test/sim/micropython/run.sh
@@ -24,7 +24,7 @@ make -C micropython/ports/hazard3-tests -j$(nproc)
 
 cd micropython/tests
 MICROPY_TEST_TIMEOUT=300 ./run-tests.py \
-	-t "exec:../../../tb_verilator/tb --bin ../ports/hazard3-tests/build/firmware.bin" \
-	-d basics -j$(nproc) \
+	-t "exec:../../../tb_verilator/tb --cpuret --bin ../ports/hazard3-tests/build/firmware.bin" \
+	-j$(nproc) \
 	|| ./run-tests.py --print-failures
 
