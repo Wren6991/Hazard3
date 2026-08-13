49ccce5ec94a3248cbf8368b68b303bb654e040d
Luke Wren <wren6991@gmail.com>
2026-08-13 21:16:21 +0100

    Get MicroPython internal benchmarks running

---
+++ test/sim/micropython/run-bench.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+CLEAN=0
+for arg in "$@"; do
+	case ${arg} in
+	--clean)
+		CLEAN=1
+		;;
+	*)
+		echo "Unrecognised arg '${arg}'"
+		exit 1
+		;;
+	esac
+done
+
+make -C ../tb_verilator -j$(nproc)
+make -C micropython/ports/hazard3-tests submodules
+if [[ ${CLEAN} == 1 ]]; then
+	make -C micropython/ports/hazard3-tests clean
+fi
+make -C micropython/ports/hazard3-tests -j$(nproc)
+
+cd micropython/tests
+MICROPY_TEST_TIMEOUT=1200 ./run-internalbench.py \
+	-t "exec:../../../tb_verilator/tb --cpuret --bin ../ports/hazard3-tests/build/firmware.bin"
+
